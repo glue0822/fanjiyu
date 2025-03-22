d4b476f4a55188c88830535b00d3c1c4d880a9bf
glue0822 <25328242@QQ.com>
2025-03-22 14:32:54 +0800

    Add files via upload

---
+++ file.py
@@ -0,0 +1,14 @@
+import os
+import sys
+from time import sleep
+print('反极域系统启动中...\n启动完成。\n@CopyRight 235工作室版权所有')
+while True:
+    print('1:解除老师控制\n2:退出')
+    do_code=input('请输入控制码:')
+    if do_code == '1':
+        error_code=os.system('taskkill /f /im StudentMain.exe')
+        if error_code != 128:
+            print('操作成功!')
+        print('可能需要再次输入1')
+    elif do_code == '2':
+        sys.exit()
