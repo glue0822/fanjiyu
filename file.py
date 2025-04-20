0fdc1e60536eb300d51a732a0ad6e61271a54682
glue0822 <25328242@QQ.com>
2025-04-20 12:32:57 +0800

    Now the file.py has GUI

--- file.py
+++ file.py
@@ -1,11 +1,14 @@
-#github地址:https://www.github.com/glue0822/no_config
-#开源项目,欢迎参加!
+from tkinter import *
 import os,sys
 process_name=['RCServer.exe','RCClient.exe','RCMonitor.exe','RCUpdate.exe','RCData.exe','RCAudio.exe','StudentMain.exe']
-print('反极域系统启动中...\n启动完成。')
-while True:
-    print('1:解除老师控制\n2:刷新DNS缓存\n3:退出')
-    do_code=input('请输入控制码:')
+
+window=Tk()
+screen=Canvas(window,width=150,height=100)
+screen.pack()
+screen.create_text(75,50,text='反极域系统\n1:解除老师控制\n2:刷新DNS缓存\n3:退出\n请点击控制键:')
+window.title('file.exe')
+
+def control(do_code):
     if do_code == '1':
         for _ in range(2):
             for i in process_name:
@@ -14,3 +17,11 @@ while True:
         os .system('ipconfig /flushdns')
     elif do_code == '3':
         sys.exit()
+    return 0
+button1=Button(window,width=20,text='1',command=lambda:control('1'))
+button2=Button(window,width=20,text='2',command=lambda:control('2'))
+button3=Button(window,width=20,text='3',command=lambda:control('3'))
+button1.pack()
+button2.pack()
+button3.pack()
+window.mainloop()
