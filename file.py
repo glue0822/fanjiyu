688acc9ec44f9a4a1d94b1f14aac58ec5c2182bd
glue0822 <25328242@QQ.com>
2025-04-24 19:36:27 +0800

    Reduced code

--- file.py
+++ file.py
@@ -6,9 +6,8 @@ window=Tk()
 screen=Canvas(window,width=150,height=100)
 window.attributes('-topmost',1)
 screen.pack()
-screen.create_text(75,50,text='反极域系统\n1:解除老师控制\n2:刷新DNS缓存\n3:退出\n请点击控制键:')
-window.title('file.exe')
-
+window.title('file.exe显示:')
+screen.create_text(75,50,text='反极域系统\n请点击控制键:')
 def control(do_code):
     if do_code == '1':
         for _ in range(2):
@@ -19,9 +18,9 @@ def control(do_code):
     elif do_code == '3':
         window.destroy()
     return 0
-button1=Button(window,width=20,text='1',command=lambda:control('1'))
-button2=Button(window,width=20,text='2',command=lambda:control('2'))
-button3=Button(window,width=20,text='3',command=lambda:control('3'))
+button1=Button(window,width=20,text='解除老师控制',command=lambda:control('1'))
+button2=Button(window,width=20,text='刷新DNS缓存',command=lambda:control('2'))
+button3=Button(window,width=20,text='关闭此页面',command=lambda:control('3'))
 button1.pack()
 button2.pack()
 button3.pack()
